8f9ba650114a7442854482cb585ce7644e8e3e1d
Elena Zhao <elena@Elenas-MacBook-Air.local>
2022-09-10 10:43:25 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Web Dev!
+          Welcome to Web Dev!!!
         </p>
         <a
           className="App-link"
