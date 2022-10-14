bbc4e42f2d9d6f1a12700f460dd202da65469376
Elena Zhao <elena@Elenas-MacBook-Air.local>
2022-10-14 13:31:38 -0700

    complete a5

--- public/tuiter/NavigationSidebar/index.js
+++ public/tuiter/NavigationSidebar/index.js
@@ -1,43 +1,71 @@
-const NavigationSidebar = (active) => {
+const NavigationSidebar = () => {
   return(`
-   <div class="list-group">
-     <a class="list-group-item" href="/">
+   <div class="list-group" id="navbar">
+     <a class="list-group-item" href="/" id="tuiter">
        <i class="fab fa-twitter"></i></a>
-      <a href="../HomeScreen/index.html" class="list-group-item list-group-item-action" id="event-target">
+      <a href="../HomeScreen/index.html" class="list-group-item list-group-item-action" id="homescreen">
         <i class="fas fa-home"></i>
         <div class="d-none d-xl-inline">Home</div>
       </a>
-      <a href="../ExploreScreen/index.html" class="list-group-item list-group-item-action" id="event-target">
+      <a href="../ExploreScreen/index.html" class="list-group-item list-group-item-action" id="explorescreen">
         <i class="fas fa-hashtag"></i>
         <div class="d-none d-xl-inline">Explore</div>
       </a>
-      <a href="notifications.html" class="list-group-item list-group-item-action" id="event-target">
+      <a href="notifications.html" class="list-group-item list-group-item-action" id="notifications">
         <i class="fas fa-bell"></i>
         <div class="d-none d-xl-inline">Notifications</div>
       </a>
-      <a href="messages.html" class="list-group-item list-group-item-action" id="event-target">
+      <a href="messages.html" class="list-group-item list-group-item-action" id="messages">
         <i class="fas fa-envelope"></i>
         <div class="d-none d-xl-inline">Messages</div>
       </a>
-      <a href="../bookmarks/index.html" class="list-group-item list-group-item-action" id="event-target">
+      <a href="../bookmarks/index.html" class="list-group-item list-group-item-action" id="bookmarks">
         <i class="fas fa-bookmark"></i>
         <div class="d-none d-xl-inline">Bookmarks</div>
       </a>
-      <a href="lists.html" class="list-group-item list-group-item-action" id="event-target">
+      <a href="lists.html" class="list-group-item list-group-item-action" id="lists">
         <i class="fas fa-list"></i>
         <div class="d-none d-xl-inline">Lists</div>
       </a>
-      <a href="../profile.html" class="list-group-item list-group-item-action" id="event-target">
+      <a href="../profile.html" class="list-group-item list-group-item-action" id="profile">
         <i class="fas fa-user"></i>
         <div class="d-none d-xl-inline">Profile</div>
       </a>
-      <a href="more.html" class="list-group-item list-group-item-action" id="event-target">
+      <a href="more.html" class="list-group-item list-group-item-action" id="more">
         <span class="position-relative">
           <i class="fas fa-circle"></i>
           <i class="fas fa-ellipsis-h position-absolute wd-more-icon fa-xs" style="color: white"></i>
         </span>
         <div class="d-none d-xl-inline">More</div>
       </a>
+      
+      <script>
+        if (window.location.pathname.endsWith("/")) {
+          $('#tuiter').addClass('active');
+        }
+        if (window.location.pathname.endsWith("HomeScreen/index.html")) {
+          $('#homescreen').addClass('active');
+        }
+        if (window.location.pathname.endsWith("ExploreScreen/index.html")) {
+          $('#explorescreen').addClass('active');
+        }
+        if (window.location.pathname.endsWith("notifications.html")) {
+          $('#notifications').addClass('active');
+        }
+        if (window.location.pathname.endsWith("bookmarks/index.html")) {
+          $('#bookmarks').addClass('active');
+        }
+        if (window.location.pathname.endsWith("lists.html")) {
+          $('#lists').addClass('active');
+        }
+        if (window.location.pathname.endsWith("profile.html")) {
+          $('#profile').addClass('active');
+        }
+        if (window.location.pathname.endsWith("more.html")) {
+          $('#more').addClass('active');
+        }
+      </script>
+
    </div>
    <div class="d-grid mt-2">
      <a href="tweet.html"
@@ -47,11 +75,3 @@ const NavigationSidebar = (active) => {
  `);
 }
 export default NavigationSidebar;
-
-const handleEventTarget = (event) => {
-  const target = event.target;
-  console.log(target);
-  $(target).css('background-color', 'blue');
-}
-const eventTarget = $("#event-target");
-eventTarget.click(handleEventTarget);
