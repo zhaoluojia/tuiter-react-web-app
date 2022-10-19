7df9e5b6f1c62c8a08a2a56a6b5cb476fa1adf8d
Elena Zhao <elena@Elenas-MacBook-Air.local>
2022-10-19 00:13:26 -0700

    complete a6 lab

---
+++ src/labs/a6/index.js
@@ -0,0 +1,21 @@
+import Nav from "../../nav";
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput from "./conditional-output";
+import TodoItem from "./todo/todo-item";
+import TodoList from "./todo/todo-list";
+
+function Assignment6() {
+  return (
+      <div>
+        <Nav/>
+        <h1>Assignment 6</h1>
+        <TodoList/>
+        <TodoItem/>
+        <ConditionalOutput/>
+        <Styles/>
+        <Classes/>
+      </div>
+  );
+}
+export default Assignment6;
