7c7e6dc7b9553785f371089c5fceefe2bb2440bc
Elena Zhao <elena@Elenas-MacBook-Air.local>
2022-10-27 10:58:22 -0700

    complete a7-lab

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -7,25 +7,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import "bootswatch/dist/cyborg/bootstrap.min.css";
 import {Routes, Route} from "react-router";
+import whoReducer from "./reducers/who-reducer";
+import tuitsReducer from "./tuits/tuits-reducer";
+import { configureStore } from '@reduxjs/toolkit';
+import {Provider} from "react-redux";
+const store = configureStore({
+  reducer: {who: whoReducer, tuits: tuitsReducer}});
 
 function Tuiter() {
   return (
-      <div className="row mt-2">
-        <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-          <NavigationSidebar active="explore"/>
+      <Provider store={store}>
+        <div className="row mt-2">
+          <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+            <NavigationSidebar active="explore"/>
+          </div>
+          <div className="col-10 col-md-10 col-lg-7 col-xl-6"
+               style={{"position": "relative"}}>
+            <Routes>
+              <Route index element={<HomeComponent/>}/>
+              <Route path="home" element={<HomeComponent/>}/>
+              <Route path="explore" element={<ExploreComponent/>}/>
+            </Routes>
+          </div>
+          <div className="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+            <WhoToFollowList/>
+          </div>
         </div>
-        <div className="col-10 col-md-10 col-lg-7 col-xl-6"
-             style={{"position": "relative"}}>
-          <Routes>
-            <Route index element={<HomeComponent/>}/>
-            <Route path="home" element={<HomeComponent/>}/>
-            <Route path="explore" element={<ExploreComponent/>}/>
-          </Routes>
-        </div>
-        <div className="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-          <WhoToFollowList/>
-        </div>
-      </div>
+      </Provider>
   );
 }
 
