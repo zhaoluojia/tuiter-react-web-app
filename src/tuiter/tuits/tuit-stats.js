073ebed12cf143402f9ec40c3aa02d08964daf89
Elena Zhao <elena@Elenas-MacBook-Air.local>
2022-11-15 23:50:14 -0800

    modify a9

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -10,27 +10,27 @@ const TuitStats = (
   const dispatch = useDispatch();
   return (
     <div className="row mt-2">
-      <div className="col col-3">
+      <div className="col col-2">
         <FaRegComment />
         <span className="ms-2">{tuit.replies}</span>
       </div>
-      <div className="col col-3">
+      <div className="col col-2">
         <FaRetweet />
         <span className="ms-2">{tuit.retuits}</span>
       </div>
       <div className="col col-3">
-        {tuit.likes}
         <i onClick={() => dispatch(updateTuitThunk({
           ...tuit,
           likes: tuit.likes + 1
         }))} className="bi bi-heart-fill me-2 text-danger"></i>
+        <span className="ms-2">{tuit.likes}</span>
       </div>
-      <div className="col col-2">
-        {tuit.dislikes}
+      <div className="col col-3">
         <i onClick={() => dispatch(updateTuitThunk({
           ...tuit,
           dislikes: tuit.dislikes + 1
         }))} className="bi bi-hand-thumbs-down-fill me-2"></i>
+        <span className="ms-2">{tuit.dislikes}</span>
       </div>
       <div className="col col-2">
         <RiShareBoxFill />
